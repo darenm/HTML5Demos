85c0f31eeb2c0c788b6e456d10ac1a367e61dfcd
Daren May <daren.may@gmail.com>
2013-04-07 19:24:46 -0600

    adding gemfile.lock

--- public/javascripts/logo.js
+++ public/javascripts/logo.js
@@ -8,7 +8,7 @@
 
 window.addEventListener(
     'load',
-    function() {
+    function () {
         CAAT.Module.Initialization.init(
             800, 500,
             'canvas',
@@ -22,7 +22,7 @@ window.addEventListener(
 
 function scene1(director) {
     var slide = director.createScene();
-    slide.setFillStyle('#000');
+    slide.setFillStyle('#FFFFFF');
 
     var rows = 4;
     var columns = 16;
@@ -36,9 +36,70 @@ function scene1(director) {
     var xoff = (slide.width - logo_ci.width) / 2;
     var yoff = (slide.height - logo_ci.height) / 2;
 
-    for ( i = 0; i < rows; i++ ) {
-        for ( j = 0; j < columns; j++) {
-            var actor = new CAAT.Foundation.Actor();
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
+            var actor = new CAAT.Foundation.Actor().
+                setBackgroundImage(logo_ci.getRef(), true).
+                setSpriteIndex(j + i * columns).
+                setLocation(-100, -100);
+
+            var bc = new CAAT.Behavior.ContainerBehavior().
+                setFrameTime(0, 23000).
+                setCycle(true);
+
+            var path1 = new CAAT.PathUtil.Path().setCubic(
+                Math.random() < .5 ? slide.width + Math.random() * 50 : -50 - Math.random() * slide.width,
+                Math.random() < .5 ? slide.width + Math.random() * 50 : -50 - Math.random() * slide.height,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.width,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.height,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.width,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.height,
+                xoff + j * logo_ci.singleWidth,
+                yoff + i * logo_ci.singleHeight
+            );
+
+            var b1 = new CAAT.Behavior.PathBehavior().
+                setFrameTime(Math.random() * 2000, 5000 + Math.random() * 2000).
+                setValues(
+                    path1
+                ).addListener({
+                    behaviorExpired: function (behavior, time, actor) {
+                        behavior.path.pathSegments[0].curve.coordlist[0].set(
+                            Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.width,
+                            Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.height
+                        )
+                    }
+                });
+
+            var path2 = new CAAT.PathUtil.Path().setCubic(
+                xoff + j * logo_ci.singleWidth,
+                yoff + i * logo_ci.singleHeight,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.width,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.height,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.width,
+                (Math.random() < .5 ? 1 : -1) * Math.random() * slide.height,
+                Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.width,
+                Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.height
+            );
+
+            var b2 = new CAAT.Behavior.PathBehavior().
+                setFrameTime(15000 + Math.random() * 2000, 5000).
+                setValues(
+                    path2
+                ).addListener({
+                    behaviorExpired: function (behavior, time, actor) {
+                        behavior.path.pathSegments[0].curve.coordlist[3].set(
+                            Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.width,
+                            Math.random() < .5 ? slide.width + Math.random() * 50 : -20 - Math.random() * slide.height
+                        )
+                    }
+                });
+
+            bc.addBehavior(b1);
+            bc.addBehavior(b2);
+
+            actor.addBehavior(bc);
+            slide.addChild(actor);
         }
     }
 
--- public/javascripts/template.js
+++ public/javascripts/template.js
@@ -15,15 +15,15 @@ CAAT.Module.Initialization.init = function(width, height, canvasId, imageURL, on
         height || 600,
         canvasContainer);
 
-    var scene = director.createScene();
-
-    var circle = new CAAT.ShapeActor().
-        setLocation(20, 20).
-        setSize(60, 60).
-        setFillStyle('#ff0000').
-        setStrokeStyle('#000000');
-
-    scene.addChild(circle);
+//    var scene = director.createScene();
+//
+//    var circle = new CAAT.ShapeActor().
+//        setLocation(20, 20).
+//        setSize(60, 60).
+//        setFillStyle('#ff0000').
+//        setStrokeStyle('#000000');
+//
+//    scene.addChild(circle);
 
     director.loop(1);
 
